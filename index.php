a918f1b7008f0b1a21b41f2395d4ff52462b5eeb
Baptiste Mabille <baptiste.mabille@viacesi.fr>
2018-03-12 15:08:03 +0100

    Creation des fichiers .inc

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-include "./include/header.php";
-include "./include/footer.php";
 
+include "./include/header.php";
+
+$page = $_GET['page'] ?? "";
+$page = "./include/" . $page . ".inc.php";
+
+$files = glob("./include/*.inc.php");
+
+if(in_array($page, $files))
+    include $page;
+else
+    include "./include/home.inc.php";
+
+include "./include/footer.php";
