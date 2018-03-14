54d20057b1b69fc04d39519e2b588044aca6d157
Baptiste Mabille <baptiste.mabille@viacesi.fr>
2018-03-14 09:00:31 +0100

    Reprise du git de fred

--- index.php
+++ index.php
@@ -1,34 +1,28 @@
 <?php
-ini_set('smtp port', 1025);
+ini_set('smtp_port', 1025);
 include "./functions/classAutoLoader.php";
-spl_autoload_register("classAutoLoader");
+spl_autoload_register('classAutoLoader');
 ?>
-
-
 <!DOCTYPE html>
 <html lang="fr-FR">
 <head>
-    <meta charset="utf-8"/>
-    <title>CESI AP - Blog</title>
-    <link rel="stylesheet" href="assets/css/style.css"
+    <meta charset="utf-8" />
+    <title>CESI AP - Blog/e-commerce</title>
+    <link rel="stylesheet" href="./assets/css/style.css" />
 </head>
 <body>
 <div id="container">
 
     <?php
-    include "./include/header.php";
-
+    include "./includes/header.php";
     $page = $_GET['page'] ?? "";
-    $page = "./include/" . $page . ".inc.php";
-
-    $files = glob("./include/*.inc.php");
-
-    if(in_array($page, $files))
+    $page = "./includes/" . $page . ".inc.php";
+    $files = glob("./includes/*.inc.php");
+    if (in_array($page, $files))
         include $page;
     else
-        include "./include/home.inc.php";
-
-    include "./include/footer.php";
+        include "./includes/home.inc.php";
+    include "./includes/footer.php";
     ?>
 </div>
 </body>
